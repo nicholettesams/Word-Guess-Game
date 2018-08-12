434c4d3aff4464236b1a3b2f49b25f6dab51cf8e
nicholettesams <nicholettesams@msn.com>
2018-08-11 20:21:30 -0400

    getRandomWord

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
-var words = ["Pearl Jam", "Alanis Morissette", "Matchbox 20", "Blind Melon", "The Offspring", "Foo Fighters", "Nine Inch Nails", "Stone Temple Pilots", "Vanilla Ice", "Savage Garden", "The Verve", "Nirvana", "Radiohead", "Mariah Carey", "Puff Daddy", "Oasis", "OutKast", "Green Day", "TLC", "Boys II Men", "Backstreet Boys", "NSync", "Ace of Base", "Weezer", "Snoop Dogg", "Dr. Dre", "Ice Cube", "Beastie Boys", "Run DMC", "Salt N Pepa", "Naughty By Nature", "Digital Underground"]
+var wordsArr = ["Pearl Jam", "Alanis Morissette", "Matchbox 20", "Blind Melon", "The Offspring", "Foo Fighters", "Nine Inch Nails", "Stone Temple Pilots", "Vanilla Ice", "Savage Garden", "The Verve", "Nirvana", "Radiohead", "Mariah Carey", "Puff Daddy", "Oasis", "OutKast", "Green Day", "TLC", "Boys II Men", "Backstreet Boys", "NSync", "Ace of Base", "Weezer", "Snoop Dogg", "Dr. Dre", "Ice Cube", "Beastie Boys", "Run DMC", "Salt N Pepa", "Naughty By Nature", "Digital Underground"]
 
-
+//Input: accepts a word as a string
+//Output: returns a string of underscores and spaces representing the word 
 var printBlanks = function(word){
     var len = word.lenth;
     var blankWord = "";
@@ -14,6 +15,12 @@ var printBlanks = function(word){
         };
         //put a space between all the letters
         blankWord = blankWord + " ";
+    }
+
+    //Input: accepts an array
+    //Output: returns the text of a random element in the array
+    var getRandomWord = function(arr){
+        return arr[Math.floor(Math.random() * arr.lenth)];
     }
 
 }
