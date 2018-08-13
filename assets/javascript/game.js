c7b53c9a6cae9f703e9f2a1f83ba910162b736b7
nicholettesams <nicholettesams@msn.com>
2018-08-13 16:37:49 -0400

    resetGame, updateHTML, key validation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,7 @@
+/*****************************/
+/*     Global Variables      */
+/*****************************/
+
 var wordsArr = ["pearl jam", "alanis morissette", "matchbox 20", "blind melon", "the offspring", "foo fighters", "nine inch nails", "stone temple pilots", "vanilla ice", "savage garden", "the verve", "nirvana", "radiohead", "mariah carey", "puff daddy", "oasis", "outkast", "green day", "tlc", "boys ii men", "backstreet boys", "nsync", "ace of base", "weezer", "snoop dogg", "dr dre", "ice cube", "beastie boys", "run dmc", "salt n pepa", "naughty by nature", "digital underground"]
 var numGuesses = 12; //start with a default of 12 guesses
 var guessedLetters = [];
@@ -6,6 +10,10 @@ var randomWord = "";
 var randomWordBlank = "";
 var wins = 0;
 var losses = 0;
+
+/*****************************/
+/*     Functions             */
+/*****************************/
 
 var outputData = function(){
     console.log("Guesses:" + numGuesses);
@@ -41,9 +49,39 @@ var getRandomWord = function(arr){
 
 //Input: string, index of char you want to replace, char is is being replaced with
 //Output: new string with replaced char
-function setCharAt(str,index,chr) {
+var setCharAt = function(str,index,chr) {
     if(index > str.length-1) return str;
     return str.substr(0,index) + chr + str.substr(index+1);
+}
+
+//Will reset to default values except for win and losses
+var resetGame = function(){
+    numGuesses = 12; 
+    guessedLetters = [];
+    guess = "";
+    randomWord = "";
+    randomWordBlank = "";    
+    updateHTML("game-text", "........", false, false);
+    updateHTML("guesses-left", numGuesses, false, false);
+    updateHTML("letters-guessed", "", false, false);
+
+}
+
+var updateHTML = function(tag, value, append, addSpaces){
+    var element = document.getElementById(tag);
+    var tempValue = "";
+
+    if (addSpaces){
+        for(var k=0; k < value.length; k++){
+            tempValue = tempValue + value[k] + " ";
+        }
+    } else if (append){
+        tempValue = element.textContent + value;
+    } else {
+        tempValue = value;
+    }
+
+    element.textContent = tempValue;
 }
 
 outputData();
@@ -66,31 +104,36 @@ document.onkeyup = function(event) {
    
         //Display random word as blanks to the screen
         randomWordBlank = printBlanks(randomWord);
-        var gameText = document.getElementById("game-text");
-        gameText.textContent = randomWordBlank;
+
+        updateHTML("game-text", randomWordBlank, false, true);
         outputData();
     } 
 
     //check to see if letter has already been guessed
     if (guessedLetters.includes(guess)){
         //end turn, don't decrease guesses or update anything
+        console.log("letter has already been guessed");
         return; 
+    } 
+    //check to see if letter or number
+    var myRegEx  = /[^a-z\d]/;
+    var isValid = !(myRegEx.test(guess));
+    if (!isValid) {
+        console.log("not a valid key");
+        return;
     }
 
-    //update the page with the letter guessed and the array
+    //update the array and page with the letter guessed
     guessedLetters.push(guess);
-    var lettersGuessed = document.getElementById("letters-guessed");
-    lettersGuessed.textContent = lettersGuessed.textContent + guess;
+    updateHTML("letters-guessed", guess, true, false);
     
-
     //check to see if the guessed letter exists in the random word
     //if it does, fill the letter(s) in instead of the underscore    
     if (randomWord.indexOf(guess) === -1) {
         console.log("letter is not in the word");
         //decrease the number of guesses left and update page
         numGuesses--;
-        var guessesLeft = document.getElementById("guesses-left");
-        guessesLeft.textContent = numGuesses;
+        updateHTML("guesses-left", numGuesses, false, false);
     } else{
         console.log("letter is in the word");
         //need to update randomWordBlank and output to page
@@ -99,36 +142,32 @@ document.onkeyup = function(event) {
                 randomWordBlank = setCharAt(randomWordBlank, i, guess)
             } 
         }
-        console.log("New randomWordBlank:" + randomWordBlank)
-        gameText = document.getElementById("game-text");
-        gameText.textContent = randomWordBlank;
+        updateHTML("game-text", randomWordBlank, false, true);
     }
 
     //Check to see if user has won.
     //Update win score
     //if no more underscores in the blank word
     if (randomWordBlank.indexOf("_") === -1){
-        alert("You won!");
+        alert("You won! \nThe correct word is '" + randomWord + "'.");
         wins++;
 
-        var numWins = document.getElementById("num-wins");
-        numWins.textContent = wins;
+        updateHTML("num-wins", wins, false, false);
 
-        //Refresh the page to reset the game
-        location.reload();
+        //reset the game
+        resetGame();
     }
 
     //Check to see if user has lost
     //Update Loss score
     if (numGuesses === 0){
-        alert("You lost!");
+        alert("You lost!  \nThe correct word is '" + randomWord + "'.");
         losses++;
 
-        var numLosses = document.getElementById("num-losses");
-        numLosses.textContent = wins;
+        updateHTML("num-losses", losses, false, false);
 
-        //Refresh the page to reset the game
-        location.reload();
+        //reset the game
+        resetGame();
     }
 
     
